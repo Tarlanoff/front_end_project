6af6ff0d833d7170d3cba8a91022f47e795ba2f3
Tarlanoff <tamerlanzt@code.edu.az>
2019-04-30 15:50:32 +0400

    owl carousels upd

--- js/script.js
+++ js/script.js
@@ -63,9 +63,102 @@ $(document).ready(function () {
     // event end //
 
 
+    // main slide carousel start
+    $('#main-slide .items').owlCarousel({
+        loop: true,
+        autoWidth: false,
+        margin: 0,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
 
 
+    ResetText();
+    TextAnimation();
+    var owl = $('#main-slide .owl-carousel');
+    owl.owlCarousel();
+
+    owl.on('translated.owl.carousel', function (event) {
+        TextAnimation();
+    });
+    owl.on('change.owl.carousel', function (event) {
+        ResetText();
+    });
 
 
+    function ResetText() {
+        $("#main-slide h1").css({
+            top: '30%',
+            opacity: '0'
+        });
+        $("#main-slide h4").css({
+            top: '60%',
+            opacity: '0'
+        });
+        $("#main-slide ul").css({
+            top: '70%',
+            opacity: '0'
+        });
+    }
 
+    function TextAnimation() {
+
+        $("#main-slide .items .active h1").animate({
+            top: '40%',
+            opacity: '1'
+        }, 500);
+        $("#main-slide .items .active h4").animate({
+            top: '50%',
+            opacity: '1'
+        }, 900);
+        $("#main-slide .items .active ul").animate({
+            top: '60%',
+            opacity: '1'
+        }, 900);
+
+    }
+    // main slide carousel end
+
+
+    // popular categories slide carousel start
+    $("#popular .items").owlCarousel({
+        loop: true,
+        margin: 15,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 3
+            },
+            600: {
+                items: 4
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+    // popular categories slide carousel end
 });
+
